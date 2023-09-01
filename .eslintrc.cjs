556d11b156f2adc197b18226277a7753abb1842e
Sergey-10dev <Sergey-10dev@gmail.com>
2023-09-01 22:32:42 +0300

    feat: implement typed hooks for dispatch and useSelector

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 'off',
   },
 }
